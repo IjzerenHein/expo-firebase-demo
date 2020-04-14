641fbb4cca83bd9dbd3cc97a23d566f16165032e
Hein Rutjes <hrutjes@gmail.com>
2020-04-14 11:55:22 +0200

    Uneject & upgrade to SDK 37

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "expe1a18d853b7148a3804ce28e93c3a1a5://192.168.1.10:19000";
-
-}
