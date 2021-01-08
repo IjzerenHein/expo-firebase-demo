ca1f752c28e603d7cb1a904e9da785976c6d6c65
Hein Rutjes <hrutjes@gmail.com>
2021-01-08 14:35:20 +0100

    cleanup analytics tests

--- src/tests/FirebaseAnalytics.js
+++ src/tests/FirebaseAnalytics.js
@@ -5,41 +5,6 @@ export const name = "FirebaseAnalytics";
 
 export async function test({ describe, beforeAll, afterAll, it, expect }) {
   describe(name, () => {
-    /*beforeAll(async () => {
-      await Analytics.deleteDefaultApp();
-
-      await Analytics.initializeAppDangerously(
-        Platform.select({
-          //   android: require('bare-expo/google-services.json'),
-          ios: {
-            CLIENT_ID: '1026763265415-4t723ioaqvjgbipp72ojp81hu529on4j.apps.googleusercontent.com',
-            REVERSED_CLIENT_ID:
-              'com.googleusercontent.apps.1026763265415-4t723ioaqvjgbipp72ojp81hu529on4j',
-            API_KEY: 'AIzaSyBgH9mo9R8WGN4tcuf7M3HZHVj44zSDvs4',
-            GCM_SENDER_ID: '1026763265415',
-            PLIST_VERSION: '1',
-            BUNDLE_ID: 'dev.expo.Payments',
-            PROJECT_ID: 'test-suite-ecd20',
-            STORAGE_BUCKET: 'test-suite-ecd20.appspot.com',
-            IS_ADS_ENABLED: false,
-            IS_ANALYTICS_ENABLED: true,
-            IS_APPINVITE_ENABLED: true,
-            IS_GCM_ENABLED: true,
-            IS_SIGNIN_ENABLED: true,
-            GOOGLE_APP_ID: '1:1026763265415:ios:d138a28b64768c9ee8af22',
-            DATABASE_URL: 'https://test-suite-ecd20.firebaseio.com',
-          },
-        })
-      );
-      await Analytics.resetAnalyticsData();
-      await Analytics.setAnalyticsCollectionEnabled(true);
-      console.log('Bundle: ', Analytics.getBundledGoogleServicesConfig());
-    });
-
-    afterAll(async () => {
-      await Analytics.deleteDefaultApp();
-    });*/
-
     describe("logEvent()", async () => {
       it(`runs`, async () => {
         let error = null;
@@ -94,17 +59,6 @@ export async function test({ describe, beforeAll, afterAll, it, expect }) {
         expect(error).toBeNull();
       });
     });
-    /* describe("setUserProperty()", async () => {
-      it(`runs`, async () => {
-        let error = null;
-        try {
-          await firebase.analytics().setUserProperty("likes_tests", "true");
-        } catch (e) {
-          error = e;
-        }
-        expect(error).toBeNull();
-      });
-    }); */
     describe("setUserProperties()", async () => {
       it(`runs`, async () => {
         let error = null;
